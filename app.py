8ce222d13c60ca598e05f47b6fa113bfb1d05ae7
claris <clarisisidore@gmail.com>
2025-03-06 09:27:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,4 +154,4 @@ def upload():
 def serve_result(filename):
     return send_from_directory(app.config['RESULT_FOLDER'], filename)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
